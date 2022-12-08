9210b7f2b9330c1f380d5c3bd54907e7f53178cc
Guilherme M. Miranda <alchemist.software@proton.me>
2022-12-08 11:34:48 -0300

    changes

--- init.bash
+++ init.bash
@@ -1,10 +1,11 @@
 # the alkemist's bashrc addition to debian's default
 
+# runtime and tools
 export EDITOR="/bin/vim"
 export DENO_INSTALL="$HOME/.deno"
 export PATH="$DENO_INSTALL/bin:$PATH"
+
+# git related; prompt, and git ignore;
 source "$HOME/code/lambda-alchemist/git.bash"
-
 PS1='${debian_chroot:+($debian_chroot)}\[\033[01;34m\]\W\[\033[00m\]$(__git_ps1 " (%s)") → '
-
 function gi() { curl -sL https://www.toptal.com/developers/gitignore/api/$@ ;}
