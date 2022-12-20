6e1d14aa27e338187e633194fbd0efcdfb330ecc
Guilherme M. Miranda <alchemist.software@proton.me>
2022-12-20 10:55:25 -0300

    changes

--- .dotfiles/user.bash
+++ .dotfiles/user.bash
@@ -1,10 +1,13 @@
 #!/bin/usr/bash
 
-source "$HOME/.dotfiles/git.bash"
+source "$HOME/.dotfiles/git.sh"
 source "$HOME/.dotfiles/deno.bash"
 source "$HOME/.cargo/env"
 export DENO_INSTALL="$HOME/.deno"
+export DVM_DIR="$HOME/.dvm"
 export PATH="$DENO_INSTALL/bin:$PATH"
+export PATH="$DVM_DIR/bin:$PATH"
+
 
 alias edconf='ln ~/.dotfiles/.editorconfig .'
 function gi() { curl -sL https://www.toptal.com/developers/gitignore/api/$@ ;}
