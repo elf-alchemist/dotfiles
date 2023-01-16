44aa7b065894125240b53c7434883ec3d3ceda7a
Guilherme M. Miranda <alchemist.software@proton.me>
2023-01-16 09:40:38 -0300

    changes

--- .dotfiles/bash/util.sh
+++ .dotfiles/bash/util.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/bash
 
-function __curl_deno() {
-	curl -fsSL https://deno.land/x/install/install.sh | sh
-	curl -fsSL https://dvm.deno.dev | sh
-	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-}
+# function __curl_deprecated() {
+# 	curl -fsSL https://deno.land/x/install/install.sh | sh
+# 	curl -fsSL https://dvm.deno.dev | sh
+# 	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+# 	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+# }
 
 function __repo_init() {
 	ln ~/.dotfiles/.editorconfig .
 	ln ~/.dotfiles/.vscode/settings.json .vscode/
 	touch .gitignore
-	echo "vscode" > .gitignore
+	echo ".vscode" > .gitignore
 	mkdir .vscode
+	git init
 }
 
 function __gi() {
--- .dotfiles/user.sh
+++ .dotfiles/user.sh
@@ -3,6 +3,7 @@
 source "$HOME/.dotfiles/bash/bash.sh"
 source "$HOME/.dotfiles/bash/util.sh"
 source "$HOME/.dotfiles/bash/deno.sh"
+source "$HOME/.dotfiles/bash/supa.sh"
 source "$HOME/.dotfiles/bash/rustup.sh"
 source "$HOME/.dotfiles/bash/cargo.sh"
 source "$HOME/.dotfiles/sh/git.sh"
@@ -10,11 +11,12 @@ source "$HOME/.cargo/env"
 
 export DENO_INSTALL="$HOME/.deno"
 export DVM_DIR="$HOME/.dvm"
+export NVM_DIR="$HOME/.nvm"
+
 export PATH="$DENO_INSTALL/bin:$PATH"
 export PATH="$DVM_DIR/bin:$PATH"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
 PS1='\[\033[01;34m\]\W\[\033[00m\]$(__git_ps1 " (%s)") → '
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
