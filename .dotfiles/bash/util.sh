6ce87de57b5f4f6a7fe829568e79e1565644d513
Guilherme M. Miranda <alchemist.software@proton.me>
2023-01-17 12:16:09 -0300

    changes

--- .dotfiles/bash/util.sh
+++ .dotfiles/bash/util.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/bash
 
-# function __curl_deprecated() {
-# 	curl -fsSL https://deno.land/x/install/install.sh | sh
-# 	curl -fsSL https://dvm.deno.dev | sh
-# 	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-# 	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-# }
+
+alias __curl_deno="curl -fsSL https://deno.land/x/install/install.sh | sh"
+alias __curl_dvm="curl -fsSL https://dvm.deno.dev | sh"
+alias __curl_nvm="curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash"
+alias __curl_rust="curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh"
 
 function __repo_init() {
 	ln ~/.dotfiles/.editorconfig .
