a4bc18fbe4515426665ca90ae78574b86e6953d6
Guilherme M. Miranda <alchemist.software@proton.me>
2023-01-25 11:17:47 -0300

    changes

--- .dotfiles/user.sh
+++ .dotfiles/user.sh
@@ -3,20 +3,20 @@
 source "$HOME/.dotfiles/bash/bash.sh"
 source "$HOME/.dotfiles/bash/util.sh"
 source "$HOME/.dotfiles/bash/deno.sh"
+source "$HOME/.dotfiles/bash/dvm.sh"
+source "$HOME/.dotfiles/bash/nvm.sh"
 source "$HOME/.dotfiles/bash/supa.sh"
 source "$HOME/.dotfiles/bash/rustup.sh"
 source "$HOME/.dotfiles/bash/cargo.sh"
 source "$HOME/.dotfiles/sh/git.sh"
 source "$HOME/.cargo/env"
+source "$NVM_DIR/nvm.sh"
 
 export DENO_INSTALL="$HOME/.deno"
 export DVM_DIR="$HOME/.dvm"
 export NVM_DIR="$HOME/.nvm"
 
 export PATH="$DVM_DIR/bin:$DENO_INSTALL/bin:$PATH"
-
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
 PS1='\[\033[01;34m\]\W\[\033[00m\]$(__git_ps1 " (%s)") → '
 
