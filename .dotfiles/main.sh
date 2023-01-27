b13017fc112faa0d5061bf4154293d9976bbdb34
Guilherme M. Miranda <alchemist.software@proton.me>
2023-01-27 15:13:03 -0300

    changes

--- .dotfiles/main.sh
+++ .dotfiles/main.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/bash
 
 source "$HOME/.dotfiles/bash/bash.sh"
-source "$HOME/.dotfiles/bash/util.sh"
 source "$HOME/.dotfiles/bash/deno.sh"
 source "$HOME/.dotfiles/bash/dvm.sh"
 source "$HOME/.dotfiles/bash/nvm.sh"
@@ -10,6 +9,7 @@ source "$HOME/.dotfiles/bash/rustup.sh"
 source "$HOME/.dotfiles/bash/cargo.sh"
 source "$HOME/.dotfiles/sh/git.sh"
 source "$HOME/.dotfiles/sh/nvm.sh"
+# source "$HOME/.dotfiles/sh/util.sh"
 source "$HOME/.cargo/env"
 
 export DENO_INSTALL="$HOME/.deno"
