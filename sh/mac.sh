117bae49844b9af7e5743f8c4d4bd8932d079187
Guilherme M. Miranda <alchemist.software@proton.me>
2023-07-10 11:29:11 -0300

    changes

--- sh/mac.sh
+++ sh/mac.sh
@@ -1,8 +1,4 @@
-# Fig pre block. Keep at the top of this file.
-[[ -f "$HOME/.fig/shell/bashrc.pre.bash" ]] && builtin source "$HOME/.fig/shell/bashrc.pre.bash"
 
 export BASH_SILENCE_DEPRECATION_WARNING=1
 source "$HOME/.dotfiles/sh/home.sh"
 
-# Fig post block. Keep at the bottom of this file.
-[[ -f "$HOME/.fig/shell/bashrc.post.bash" ]] && builtin source "$HOME/.fig/shell/bashrc.post.bash"
