641bea6454fe22b03ddb9c5faeda0cd2b9742c0b
Guilherme M. Miranda <alchemist.software@proton.me>
2023-12-28 02:09:58 +0000

    Huh, fixed pathing

--- init.sh
+++ init.sh
@@ -1,14 +1,8 @@
 #!/usr/bin/env bash
 
 case "$(uname --all)" in
-  Linux*Debian*)
-    source "$HOME/dotfiles/config/bash/home.sh"
-    ;;
-  Darwin*)
-    source "$HOME/dotfiles/config/bash/debian.sh"
-    ;;
-  *)
-    source "$HOME/dotfiles/config/bash/darwin.sh"
-    ;;
+  Linux*Debian*) source "$HOME/dotfiles/bash/home.sh" ;;
+  Darwin*) source "$HOME/dotfiles/bash/debian.sh" ;;
+  *) source "$HOME/dotfiles/bash/darwin.sh" ;;
 esac
 
