c88af3d254fa0ddd913c22d643809f218058e452
Guilherme M. Miranda <alchemist.software@proton.me>
2024-01-24 12:02:16 +0000

    add git hex diff

--- bash/home.bash
+++ bash/home.bash
@@ -67,14 +67,14 @@ alias .4="cd ../../../.."
 export DOOMWADDIR="$XDG_DATA_HOME/games/doom";
 export DOOMWADPATH="$DOOMWADDIR:/usr/local/share/games/doom:/usr/local/share/doom:/usr/share/games/doom:/usr/share/doom";
 
-alias doom1="woof -iwad doom1 -file doom1-midi.wad doom1-sprite.wad -complevel mbf21 -fast"
-alias doom2="woof -iwad doom2 -file doom2-midi.wad doom2-sprite.wad -complevel mbf21 -fast"
+alias doom1="woof -iwad doom1 -file doom1-midi.wad doom1-sprite.wad -complevel mbf21 -coop_spawns -fast"
+alias doom2="woof -iwad doom2 -file doom2-midi.wad doom2-sprite.wad -complevel mbf21 -coop_spawns -fast"
 
 alias doomsigil1="woof -iwad doom1 -file doom1-sprite.wad sigil1.wad"
 alias doomsigil2="woof -iwad doom1 -file doom1-sprite.wad sigil1.wad sigil2.wad"
 alias doomnrftl="woof -file doom2-sprite.wad nrftl.wad"
 
-alias doom0="woof -file doom2-sprite.wad doom-zero.wad"
+alias doomzero="woof -file doom2-sprite.wad doom-zero.wad"
 alias doomttp="woof -file doom2-sprite.wad ttp.wad"
 
 # ECMAScript is pain
